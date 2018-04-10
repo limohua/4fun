85ca43dc375110a1e1ba45bc5d3222c1639ba08d
Mohua Li <lmh.limohua@gmail.com>
2018-04-10 11:38:30 +0800

    added centos sn2
    Signed-off-by: Mohua Li <lmh.limohua@gmail.com>

---
+++ centos/ali/sn2/get.sh
@@ -0,0 +1,23 @@
+rpm -qa > rpm.list
+rpm -Va >> rpm-va.output
+cat /proc/cmdline >> cmdline
+systemctl list-units >> systemd.units
+dracut --print-cmdline >> dracut.cmdline
+dracut  --list-modules >> dracut.modules
+lsinitrd  >> lsinitrd.output
+ps -ef  >> ps-ef.out
+lsmod >> lsmod.out
+sysctl -a >> sysctl.ouot
+cat /proc/mounts >> proc-mounts
+tune2fs -l /dev/vda1  >> vda1.out
+dumpe2fs /dev/vda1  >> vda1_dumpe2fs.out
+cat /proc/meminfo >> meminfo
+cat /proc/sys/vm/swappiness  >> swapiness.out
+dmesg >> demsg.out
+dmidecode >> dmidecode.out
+cat /sys/block/vda/queue/scheduler  >> ioscheduler
+ethtool -k eth0 >> ethtool.k.eth0
+ethtool -i eth0 >> ethtool.i.eth0
+lsblk >> lsblk.out
+lspci >> lspci.out
+uname -r >> unamer.out
